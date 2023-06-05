d507b1514e644406e3d1a08cde385935ad17ae94
Saara <jonessaara@gmail.com>
2023-06-05 13:51:54 +0200

    avatar creation

---
+++ SURE/Scenes/avatar_creation.gd
@@ -0,0 +1,35 @@
+extends Control
+
+var parties = [
+	"Social-democratic Party",
+	"Green Party",
+	"Green Liberal Party",
+	"Independant/No Party Affiliation",
+	"Christian-democratic Party",
+	"Liberal Party",
+	"Conservative Party",
+]
+
+var def = [
+	"State-centered, progressive, Strong environmental protection",
+	"State-centered, progressive, Strong environmental protection",
+	"Mix, progressive, Strong environmental protection",
+	"No positions",
+	"Mix, Conservative, Status Quo",
+	"Free Market, Progressive, Status Quo",
+	"Free Market, Conservative, Status Quo",
+	
+]
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_h_slider_value_changed(value):
+	$Party.text = parties[value]
+	$Def.text = def[value]
